23cbfe424d4ebb7e9cd0fe698b24d263f3d5fcfd
Davide Pignotti <davide110494@gmail.com>
2016-10-28 18:28:22 +0200

    commit

---
+++ JerseyHibernateApp/src/main/java/com/topjavatutorial/dao/EmployeeDAO.java
@@ -0,0 +1,64 @@
+package com.topjavatutorial.dao;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.topjavatutorial.EmployeeBean;
+
+public class EmployeeDAO {
+
+	public void addEmployee(EmployeeBean emp){
+
+		Session session = SessionUtil.getSession();
+		Transaction tx = session.beginTransaction();
+		addEmployee(session,emp);tx.commit();
+		session.close();
+		
+	}
+	private void addEmployee(Session session, EmployeeBean bean){
+		Employee employee = new Employee();
+		employee.setName(bean.getName());
+		employee.setAge(bean.getAge());
+		session.save(employee);
+		}
+	
+	public List<Employee> getEmployees(){
+		Session session = SessionUtil.getSession();
+		Query query = session.createQuery("from Employee");
+		List<Employee> employees =query.list();
+		return employees;
+		}
+	
+	public int deleteEmployee(int id) {
+		Session session = SessionUtil.getSession();
+		Transaction tx = session.beginTransaction();
+		String hql = "delete from Employee where id = :id";
+		Query query = session.createQuery(hql);
+		query.setInteger("id",id);
+		int rowCount = query.executeUpdate();
+		System.out.println("Rows affected: " + rowCount);
+		tx.commit();
+		session.close();
+		return rowCount;
+		}
+	
+	public int updateEmployee(int id, Employee emp){
+		if(id <=0)
+			return 0;
+		
+		Session session = SessionUtil.getSession();
+		Transaction tx = session.beginTransaction();
+		String hql = "update Employee set name = :name, age=:age where id = :id";
+		Query query = session.createQuery(hql);
+		query.setInteger("id",id);
+		query.setString("name",emp.getName());
+		query.setInteger("age",emp.getAge());
+		int rowCount = query.executeUpdate();
+		System.out.println("Rows affected: " + rowCount);
+		tx.commit();
+		session.close();
+		return rowCount;
+		}
+}
